f9c89ccf9ff28f9ad83da6e51bbdab3cb552ef19
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-11-28 12:00:00 -0600

    /api/hello

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,22 @@
+import express from "express";
+
 import { hello } from "./hello";
 
-console.log(hello());
-console.log("db url:", process.env.DATABASE_URL);
+// console.log(hello());
+// console.log("db url:", process.env.DATABASE_URL);
+
+// Optional variables
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json());
+
+// TODO serve static files -- will big files block API?
+
+app.get("/api/hello", (req: express.Request, res: express.Response) => {
+  res.send({ message: "hello, world" });
+});
+
+app.listen(PORT, () =>
+  console.log("Crunchspace server is listening at http://localhost:" + PORT)
+);
