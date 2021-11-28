e1da36b8a1779a71d92e04a6cba89feb591a760b
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-11-28 12:00:00 -0600

    Client: Add Jest

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,18 +1,4 @@
-// (base: https://www.typescriptlang.org/play/)
+import { hello } from "./hello";
 
-class Greeter {
-  greeting: string;
-  constructor(message: string) {
-    this.greeting = message;
-  }
-  greet(): string {
-    return `Hello, ${this.greeting}`;
-  }
-}
-
-const greeter = new Greeter("world");
-
-const button = document.getElementById("myButton")!;
-button.onclick = () => {
-  alert(greeter.greet());
-};
+console.log(hello());
+console.log(hello("someone"));
